a9b5667b50e3aa23c62e6b594566127431f1acb7
Christian Kau <ckau@rhrk.uni-kl.de>
2026-05-09 18:03:42 +0200

    Added function to create tables on first start

--- backend/src/db/postgres.js
+++ backend/src/db/postgres.js
@@ -3,10 +3,29 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-export const pool = new Pool({
+const pool = new Pool({
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
 })
+
+
+const res = await pool.query(
+  `CREATE TABLE IF NOT EXISTS cars (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS fuel_entries (
+  id SERIAL PRIMARY KEY,
+  car_id INTEGER REFERENCES cars(id),
+  liters NUMERIC,
+  price NUMERIC,
+  mileage INTEGER,
+  created_at TIMESTAMP DEFAULT NOW()
+);`
+)
+
+export pool
